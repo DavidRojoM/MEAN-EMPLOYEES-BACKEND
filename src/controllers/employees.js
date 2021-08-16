2990d2590586adc8382baa01840a230f6e2dd2fc
David Rojo <davidrojo1997@gmail.com>
2021-08-16 03:57:09 +0200

    CRUD finished

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -10,15 +10,19 @@ employees.createEmployee = async (req, res) => {
   const employee = new Employee(req.body)
 
   await employee.save()
-  res.send({ message: 'Employee created' })
+  res.send({ message: employee.id })
 }
+
 employees.getEmployee = async (req, res) => {
   const employee = await Employee.findById(req.params.id)
-  res.json(employee)
+  res.send(employee)
 }
-employees.updateEmployee = (req, res) => {
-  res.send('updateEmployee')
+
+employees.updateEmployee = async (req, res) => {
+  const employee = await Employee.findByIdAndUpdate(req.params.id, req.body)
+  res.send({ updated: employee.id })
 }
+
 employees.deleteEmployee = async (req, res) => {
   const employee = await Employee.findByIdAndDelete(req.params.id)
   res.json(employee)
