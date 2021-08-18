5b13ea7db5fbdd2cb907b4c00cd3774bc98ad0b0
David Rojo <davidrojo1997@gmail.com>
2021-08-18 03:03:18 +0200

    CORS DEPENDENCY

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 
 app.set('port', process.env.PORT || 3000)
 
+app.use(cors)
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
